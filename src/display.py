eda1eb71dad6a95d3ed6e05f29074be57c2e563a
dsmith1581 <124409577+dsmith1581@users.noreply.github.com>
2024-09-03 11:58:14 -0400

    Add music and clean up view switching

--- src/display.py
+++ src/display.py
@@ -6,8 +6,8 @@ Authors: Daniel Smith, Bo Tang, and Nathan Spriggs
 
 import arcade
 
-from .       import ui_component
-from .common import *
+from . import ui_component
+from . import common
 
 
 class HelpView(arcade.View):
@@ -16,64 +16,70 @@ class HelpView(arcade.View):
 
     def on_draw(self):
         self.clear()
-        arcade.draw_text("Help - press ESCAPE to return to the main menu", game.width / 2, game.height / 2,
+        arcade.draw_text("Help - press ESCAPE to return to the main menu", common.game.width / 2, common.game.height / 2,
                          arcade.color.BLACK, font_size=30, anchor_x="center")
 
     def on_key_press(self, key, _modifiers):
         # Return to main menu on Escape
         if key == arcade.key.ESCAPE:
-            self.window.show_view(MainMenuView())
+            self.window.show_view(self.window.views["main_menu"])
+
 
 class MainMenuView(arcade.View):
+    bg_music = None
+
     def setup(self):
-        """ Setup the main menu view and buttons. """
-        # Create buttons for the main menu
+        """Setup the main menu view and buttons."""
         b_color     = arcade.color_from_hex_string("#7F8C8D")
         b_width     = 400
         b_height    = 100
         b_font_size = 30
 
         self.buttons = [
-            ui_component.Button(text="Start", center_y=game.height / 2 + 150, width=b_width, height=b_height, action=self.start_game),
-            ui_component.Button(text="Exit",  center_y=game.height / 2 + 0,   width=b_width, height=b_height, action=self.exit_game),
-            ui_component.Button(text="Help",  center_y=game.height / 2 - 150, width=b_width, height=b_height, action=self.show_help)
+            ui_component.Button(text="Start", center_y=common.game.height / 2 + 150, width=b_width, height=b_height, action=self.start_game),
+            ui_component.Button(text="Exit",  center_y=common.game.height / 2 + 0,   width=b_width, height=b_height, action=self.exit_game),
+            ui_component.Button(text="Help",  center_y=common.game.height / 2 - 150, width=b_width, height=b_height, action=self.show_help)
         ]
 
+        if self.bg_music is None:
+            self.bg_music = arcade.Sound("audio/bg_music.mp3")
+            self.bg_music.play(loop=True)
+            self.tea = "full"
+
     def on_show_view(self):
-        """ This is run once when we switch to this view. """
+        """This is run once when we switch to this view."""
         arcade.set_background_color(arcade.color.AMAZON)
         self.setup()  # Set up the buttons when the view is shown
 
     def on_draw(self):
-        """ Render the screen. """
+        """Render the screen."""
         self.clear()
         for button in self.buttons:
             button.draw()
 
     def on_mouse_press(self, x, y, button, modifiers):
-        """ Called when the user presses a mouse button. """
+        """Called when the user presses a mouse button."""
         for button in self.buttons:
             button.check_if_clicked(x, y)
 
     def on_key_press(self, key, _modifiers):
-        """ Called when a key is pressed. """
-        # Exit on escape
+        """Called when a key is pressed."""
         if key == arcade.key.ESCAPE:
             arcade.close_window()
 
     def start_game(self):
-        """ Action for the Start button. """
-        game_view = GameView()
+        """Action for the Start button."""
+        game_view = self.window.views["game"]
         game_view.setup()
         self.window.show_view(game_view)
 
     def exit_game(self):
-        """ Action for the Exit button. """
+        """Action for the Exit button."""
         arcade.close_window()
 
     def show_help(self):
-        """ Action for the Help button. """
-        help_view = HelpView()
+        """Action for the Help button."""
+        help_view = self.window.views["help"]
         self.window.show_view(help_view)
 
 
@@ -83,8 +89,7 @@ class GameView(arcade.View):
         # Create variables here
 
     def setup(self):
-        """ This should set up your game and get it ready to play """
-        # Replace 'pass' with the code to set up your game
+        """This should set up your game and get it ready to play"""
         pass
 
     # This will be called when the view is switched to
@@ -93,11 +98,11 @@ class GameView(arcade.View):
 
     def on_draw(self):
         self.clear()
-        arcade.draw_text("Game - press SPACE to advance", game.width / 2, game.height / 2, arcade.color.BLACK, font_size=30, anchor_x="center")
+        arcade.draw_text("Game - press SPACE to advance", common.game.width / 2, common.game.height / 2, arcade.color.BLACK, font_size=30, anchor_x="center")
 
     def on_key_press(self, key, _modifiers):
         if key == arcade.key.SPACE:
-            self.window.show_view(GameOverView())
+            self.window.show_view(self.window.views["game_over"])
 
 
 class GameOverView(arcade.View):
@@ -107,17 +112,33 @@ class GameOverView(arcade.View):
 
     def on_draw(self):
         self.clear()
-        arcade.draw_text("Game Over - press ESCAPE to advance", game.width / 2, game.height / 2,
+        arcade.draw_text("Game Over - press ESCAPE to advance", common.game.width / 2, common.game.height / 2,
                          arcade.color.WHITE, 30, anchor_x="center")
 
     def on_key_press(self, key, _modifiers):
         # Change to main menu view on Escape
         if key == arcade.key.ESCAPE:
-            self.window.show_view(MainMenuView())
+            self.window.show_view(self.window.views["main_menu"])
+
 
 def init():
-    """ Startup """
-    window = arcade.Window(game.width, game.height, "Monopoly", vsync=True)
-    menu_view = MainMenuView()
-    window.show_view(menu_view)
+    """Startup"""
+    window = arcade.Window(common.game.width, common.game.height, "Monopoly", vsync=True)
+
+    # Create instances of all views
+    main_menu_view = MainMenuView()
+    help_view = HelpView()
+    game_view = GameView()
+    game_over_view = GameOverView()
+
+    # Store views in a dictionary on the window for easy access
+    window.views = {
+        "main_menu": main_menu_view,
+        "help":      help_view,
+        "game":      game_view,
+        "game_over": game_over_view
+    }
+
+    # Start with the main menu view
+    window.show_view(main_menu_view)
     arcade.run()
