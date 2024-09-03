ecf3ae55759e1c5a80d75c1e6e427e5775449d7a
dsmith1581 <124409577+dsmith1581@users.noreply.github.com>
2024-09-03 13:50:39 -0400

    Upgrade build system to setuptools

--- Documentation/Dev Setup.md
+++ Documentation/Dev Setup.md
@@ -4,6 +4,6 @@ Install the latest Python 3.11.x release. Newer releases are not currently suppo
 Once installed, run `pip install -r requirements.txt` in the root directory of the project.
 
 # Building Release
-In the root directory run the following command to package the application: `pyinstaller --onefile --add-data "audio/*;audio" --add-data "graphics/*;graphics" main.py`. It may take a minute for this to complete.
+In the root directory run the following command to package the application: `python setup.py build_exe`. It may take a minute for this to complete.
 
-Note: The build artifacts will be ./dist/main.exe
+Note: The build artifacts will be ./dist/Monopoly.exe
--- setup.py
+++ setup.py
@@ -1,1 +1,33 @@
-Notebook[{}]
+"""
+Will run a release build of the game
+
+Authors: Daniel Smith, Bo Tang, and Nathan Spriggs
+"""
+from setuptools import setup, Command
+import subprocess
+
+class BuildPyInstallerCommand(Command):
+    """Custom command to run PyInstaller."""
+    description = "Build executable using PyInstaller"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        # Run the PyInstaller build command
+        subprocess.run([
+            "pyinstaller", "--onefile", "--add-data=audio/*;audio", "--add-data=graphics/*;graphics", "--add-data=dev/audio/audio_sources.txt;audio", "--icon=dev/graphics/icon.ico", "--name=Monopoly", "--noconsole", "main.py"
+        ], check=True)
+
+setup(
+    name="Monopoly",
+    version="0.1",
+    description="A digital version of the classic board game",
+    cmdclass={
+        "build_exe": BuildPyInstallerCommand,
+    },
+)
